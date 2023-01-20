54ae2e0b2c8d93a778d171157d7eed48e780a3f7
MariusOpruta <opmasorin@gmail.com>
2023-01-20 20:29:42 +0200

    tema 10 update

--- Tema10.py
+++ Tema10.py
@@ -7,24 +7,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver import Keys
 
 
-chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
 #Test1
+chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 chrome.maximize_window()
 chrome.get("https://the-internet.herokuapp.com/login")
 text_welcome = chrome.find_element(By.TAG_NAME, "h2")
 assert text_welcome.text=="Login Page"
-#assert len(text_welcome)==10
+chrome.find_element(By.TAG_NAME,"h4")=="This is where you can log into the secure area. Enter  for the username and  for the password. If the information is wrong you should see error messages."
+chrome.quit()
 #Test2
+chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+chrome.maximize_window()
+chrome.get("https://the-internet.herokuapp.com/login")
 chrome.find_element(By.LINK_TEXT,"Elemental Selenium").click()
 driver = webdriver.Chrome()
 driver.execute_script("window.open('','_blank');")
 def test_url(self):
     actual = self.chrome.current_url
-    expected = 'https://the-internet.herokuapp.com/login'
+    expected = 'https://the-internet.herokuapp.com/login '
     self.assertEqual(expected, actual, 'URL-ul nu este corect')
 chrome.quit()
-#Test3
+# #Test3
 chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 chrome.maximize_window()
 chrome.get("https://the-internet.herokuapp.com/login")
@@ -33,31 +36,40 @@ username_input.send_keys("tomsmith")
 chrome.find_element(By.ID,"password").send_keys("SuperSecretPassword!")
 chrome.find_element(By.CLASS_NAME,"radius").click()
 chrome.find_element(By.CSS_SELECTOR, "[href='/logout']").click()
-assert text_welcome.text=="Login Page"
+assert chrome.current_url=="https://the-internet.herokuapp.com/login"
 chrome.quit()
-#Test4
+# #Test4
 chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 chrome.maximize_window()
 chrome.get("https://the-internet.herokuapp.com/add_remove_elements/")
 chrome.find_element(By.TAG_NAME,"button").click()
-#assert chrome.find_element(By.CLASS_NAME,"addElement()")==chrome.find_element()
-# Test5
+delete_button=chrome.find_element(By.ID,"elements")
+assert delete_button.is_displayed()
+chrome.quit()
+# # Test5
 chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 chrome.maximize_window()
 chrome.get("https://the-internet.herokuapp.com/add_remove_elements/")
 chrome.find_element(By.TAG_NAME,"button").click()
 chrome.find_element(By.CLASS_NAME,"added-manually").click()
-chrome.find_element(By.ID,"elements").is_displayed()
-chrome.quit()
+# t=chrome.find_element(By.CLASS_NAME,"added-manually").size()
+# if (t==0):
+#     print('Butonul nu exista')
+# else:
+#     print('Butonul exista')
+
+# chrome.quit()
 # Test6
 chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 chrome.maximize_window()
 chrome.get("https://the-internet.herokuapp.com/add_remove_elements/")
 button=chrome.find_element(By.TAG_NAME,"button")
-for i in range(5):
+for i in range(10):
     button.click()
-#assert chrome.find_element(By.ID,"elements").click()
-# chrome.quit()
+t = chrome.find_element(By.ID,"elements").size(10)
+assert t==10
+
+chrome.quit()
 #Test7
 chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 chrome.maximize_window()
