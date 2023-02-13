402ebaa61493c6029a57b48a7d7530babf31f03c
MariusOpruta <opmasorin@gmail.com>
2023-02-13 20:38:58 +0200

    tema 10 update 7final

--- test-tehnic/Probl2.py
+++ test-tehnic/Probl2.py
@@ -6,23 +6,50 @@ Eg Rock beat scissors, scissors beat paper, paper beat hammer, hammer beats rock
 '''
 import random
 
-game = ["rock","scissors","paper"]
+game = ["rock", "scissors", "paper", "hammer"]
 player1 = random.choice(game)
 player2 = random.choice(game)
 
-if player2==player1:
-    print("both wins")
-elif player2 == game[0] and player1 == game[1]:
-    print("player2 win")
-elif player2 == game[1] and player1 == game[2]:
-    print("player2 win")
-elif player2 == game[2] and player1 == game[0]:
-    print("player2 win")
-elif player1 == game[0] and player2 == game[1]:
-    print("player1 win")
-elif player1 == game[1] and player2 == game[2]:
-    print("player1 win")
-elif player1 == game[2] and player2 == game[0]:
-    print("player1 win")
+
+
+# for i in range(len(game)-1):
+#     if player2 == game[i] and player1 == game[i+1]:
+#         print("player2 win")
+#     elif player2 == game[3] and player1 == game[0]:
+#         print("player2 win")
+#     elif player2 ==game[i] and player1 == game[i+2]:
+#         print("player2 win")
+#     elif player1 == game[i] and player2 == game[i+1]:
+#         print("player1 win")
+#     elif player1 == game[3] and player2 == game[0]:
+#         print("player1 win")
+#     elif player1 == game[i] and player2 == game[i+2]:
+#         print("player1 win")
+#     elif player1==player2:
+#         print("both win")
 
 print(f"alegeri: player1={player1} player2={player2} ")
+
+
+
+# if player1 == player2:
+#         print("both win")
+# elif player1 == game[0] and player2 == game[1]:
+#         print("player1 win")
+# elif player1 == game[1] and player2 == game[2]:
+#         print("player1 win")
+# elif player1 == game[2] and player2 == game[3]:
+#         print("player1 win")
+# elif player1 == game[3] and player2 == game[0]:
+#         print("player1 win")
+# elif player2 == game[0] and player1 == game[1]:
+#         print("player2 win")
+# elif player2 == game[1] and player1 == game[2]:
+#         print("player2 win")
+# elif player2 == game[2] and player1 == game[3]:
+#         print("player2 win")
+# elif player2 == game[3] and player1 == game[0]:
+#         print("player2 win")
+
+
+
