a73218f0e6555ed52d7cb5b3fc482dbedb8c2a64
MariusOpruta <opmasorin@gmail.com>
2023-02-15 20:22:30 +0200

    tema 10 update 7final

--- test-tehnic/Probl1.py
+++ test-tehnic/Probl1.py
@@ -4,21 +4,29 @@ the number of floors in each building. Determine how many buildings are the tall
 '''
 
 lista_etaje = [2, 4, 5, 3, 4, 6, 3, 6, 4, 6, 2]
-for i in range(0,len(lista_etaje)-1):
-    if lista_etaje[i]<lista_etaje[i+1]:
+# for i in range(0,len(lista_etaje)-1):
+#     if lista_etaje[i]<lista_etaje[i+1]:
+#         lista_etaje.remove(lista_etaje[i])
+#     else:
+#         lista_etaje.remove(lista_etaje[i+1])
+#     if lista_etaje[i]>lista_etaje[i+1]:
+#         lista_etaje.remove(lista_etaje[i+1])
+#         lista_etaje.pop(0)
+#         lista_etaje.pop()
+#     else:
+#         lista_etaje.remove(lista_etaje[i])
+#
+#     print(f"List {lista_etaje}")
+#     print(f"cladiri {len(lista_etaje)}")
+
+for i in range(0,len(lista_etaje)):
+    if (lista_etaje[i]<lista_etaje[i+1]):
         lista_etaje.remove(lista_etaje[i])
-    else:
-        lista_etaje.remove(lista_etaje[i+1])
-    if lista_etaje[i]>lista_etaje[i+1]:
-        lista_etaje.remove(lista_etaje[i+1])
-        lista_etaje.pop(0)
-        lista_etaje.pop()
-    else:
+
+    if (lista_etaje[i]<lista_etaje[i+1]):
         lista_etaje.remove(lista_etaje[i])
+    elif(lista_etaje[i]>lista_etaje[i+1]):
+        lista_etaje.remove(lista_etaje[i+1])
 
     print(f"List {lista_etaje}")
-    print(f"cladiri {len(lista_etaje)}")
-
-
-
 
